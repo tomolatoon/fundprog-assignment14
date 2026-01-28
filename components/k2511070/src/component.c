f28741a6798a0530501cf6c1610ba755cf1f23e8
25kanaizuka <rk061@rk06151312005.localdomain>
2026-01-28 19:19:25 +0900

    とりあえず音符が動いた

--- components/k2511070/src/component.c
+++ components/k2511070/src/component.c
@@ -10,10 +10,10 @@ void k2511070_component_init(void)
 	// TODO: 初期化処理
 }
 
-//一個の八分音符っを描画する関数
+//一個の八分音符を描画する関数
 HLayer one_eighth_note(RGBA c, Point p){
 	HLayer layer = layer_create(SIZE(640,480));
-	RGBA c1;
+	
 	int dx = 0;
 	int dy = 0;
 	RGBA black = rgba_new(0.0, 0.0, 0.0, 1.0);
@@ -33,30 +33,46 @@ HLayer one_eighth_note(RGBA c, Point p){
 					 1,2,2,2,2,2,1,3,3,3,
 					 1,2,2,2,2,2,1,3,3,3,
 					 1,2,2,2,2,1,1,3,3,3,
-					 1,1,1,1,1,1,3,3,3,3,};
+					 1,1,1,1,1,1,3,3,3,3};
 
 	int i;
     for(i = 0;i < 150;i++){
         if(note[i] == 3){
 			//do nothing
 		}else if(note[i] == 0){
-			c1 = white;
+			for(int j = 0; j < 3; j++) {
+				for(int k = 0; k < 3; k++) {	
+					dx = 3 * (i % 10) + k;
+					dy = 3 * (i / 10) + j;
+				
+					Point draw_p = POINT(dx, dy);
+				
+					layer_set_pixel(layer, draw_p, white);
+				}
+			}
 		}else if(note[i] == 1){
-			c1 = black;
+			for(int j = 0; j < 3; j++) {
+				for(int k = 0; k < 3; k++) {	
+					dx = 3 * (i % 10) + k;
+					dy = 3 * (i / 10) + j;
+				
+					Point draw_p = POINT(dx, dy);
+				
+					layer_set_pixel(layer, draw_p, black);
+				}
+			}
 		}else{
-			c1 = c;
+			for(int j = 0; j < 3; j++) {
+				for(int k = 0; k < 3; k++) {	
+					dx = 3 * (i % 10) + k;
+					dy = 3 * (i / 10) + j;
+				
+					Point draw_p = POINT(dx, dy);
+				
+					layer_set_pixel(layer, draw_p, c);
+				}
+			}
 		}
-
-		for(int j = 0; j < 2; j++) {
-            for(int k = 0; k < 2; k++) {	
-                dx = 2 * (i % 10) + k;
-                dy = 2 * (i / 10) + j;
-
-                Point draw_p = POINT(p.x + dx, p.y + dy);
-
-                layer_set_pixel(layer, draw_p, c1);
-            }
-        }
 	}
 	return layer;
 }
@@ -93,12 +109,12 @@ HLayer two_eighth_note(RGBA c, Point p){
 			c1 = c;
 		}
 
-		for(int j = 0; j < 2; j++) {
-            for(int k = 0; k < 2; k++) {	
-                dx = 2 * (i % 12) + k;
-                dy = 2 * (i / 12) + j;
+		for(int j = 0; j < 3; j++) {
+            for(int k = 0; k < 3; k++) {	
+                dx = 3 * (i % 12) + k;
+                dy = 3 * (i / 12) + j;
 
-                Point draw_p = POINT(p.x + dx, p.y + dy);
+                Point draw_p = POINT(dx, dy);
 
                 layer_set_pixel(layer, draw_p, c1);
             }
@@ -109,17 +125,18 @@ HLayer two_eighth_note(RGBA c, Point p){
 
 //一つの八分音符が4つ画面上をはねるアニメーション.背景太陽のレイヤーとミクの間
 HLayer k2511070_bound_one_note_layer(double time){
+	RGBA white = rgba_new(1.0, 1.0, 1.0, 1.0);
 	double y = 270;
-	double v = 0;
-	double g = 2;
-	double x_mod;
-	int isfalling = 1;
-	double x_speed = 50;
-	double diff;
+	// double v = 0;
+	// double g = 2;
+	// int isfalling = 1;
+	double x_speed = 200;
+	// double diff;
 
 	HLayer layer = layer_create(SIZE(640,480));
+	layer_fill(layer,white);
 
-	RGBA c1 = rgba_new(1.0, 0.5, 0.0, 0.25);
+	RGBA c1 = rgba_new(1.0, 0.5, 0.25, 1.0);
 	RGBA c2 = rgba_new(1.0, 0.65, 0.0, 1.0);
 	RGBA c3 = rgba_new(0.0, 0.75, 1.0, 1.0);
 	RGBA c4 = rgba_new(0.6, 0.2, 0.8, 1.0);
@@ -129,40 +146,39 @@ HLayer k2511070_bound_one_note_layer(double time){
 	Point p3 = {320,(int)y};
 	Point p4 = {480,(int)y};
 
-	x_mod = fmod(x_speed * time, 640.0);
+	p1.x = (int)(p1.x + x_speed * time > 640.0 ? fmod(p1.x + x_speed * time,640.0) : p1.x + x_speed * time);
+	p2.x = (int)(p2.x + x_speed * time > 640.0 ? fmod(p2.x + x_speed * time,640.0) : p2.x + x_speed * time);
+	p3.x = (int)(p3.x + x_speed * time > 640.0 ? fmod(p3.x + x_speed * time,640.0) : p3.x + x_speed * time);
+	p4.x = (int)(p4.x + x_speed * time > 640.0 ? fmod(p4.x + x_speed * time,640.0) : p4.x + x_speed * time);
 
-	p1.x = (int)(p1.x + x_speed * time / 640.0 > 1 ? p1.x + x_mod : p1.x + x_speed);
-	p2.x = (int)(p2.x + x_speed * time / 640.0 > 1 ? p2.x + x_mod : p2.x + x_speed);
-	p3.x = (int)(p3.x + x_speed * time / 640.0 > 1 ? p3.x + x_mod : p3.x + x_speed);
-	p4.x = (int)(p4.x + x_speed * time / 640.0 > 1 ? p4.x + x_mod : p4.x + x_speed);
+	// isfalling = fmod((double)time * 2,2.0) == 0 ? 1 : 0;
 
-	isfalling = fmod((double)time * 2,2.0) == 0 ? 1 : 0;
-
-	if(isfalling == 1){
-		diff = fmod(time,1.0);
-		int n = (int)diff * 15;
-		for(int i = 0;i < n;i++){
-			y += v;
-			v += g / 15.0;
-		}
-	}else{
-		diff = fmod(time,1);
-		int n = (int)diff * 15;
-		for(int i = 0;i < 15 - n;i++){
-			y += v;
-			v += g / 15;
-		}
-	}
+	// if(isfalling == 1){
+	// 	diff = fmod(time,1.0);
+	// 	int n = (int)diff * 15;
+	// 	for(int i = 0;i < n;i++){
+	// 		y += v;
+	// 		v += g / 15.0;
+	// 	}
+	// }else{
+	// 	diff = fmod(time,1);
+	// 	int n = (int)diff * 15;
+	// 	for(int i = 0;i < 15 - n;i++){
+	// 		y += v;
+	// 		v += g / 15;
+	// 	}
+	// }
 
 	p1.y = (int)y;
 	p2.y = (int)y;
 	p3.y = (int)y;
 	p4.y = (int)y;
 
-	HLayer note1 = one_eighth_note(c1, p1);
-	HLayer note2 = one_eighth_note(c2, p2);
-	HLayer note3 = one_eighth_note(c3, p3);
-	HLayer note4 = one_eighth_note(c4, p4);
+	layer_fill(layer,white);
+	HLayer note1 = one_eighth_note(c1, POINT(0, 0));
+	HLayer note2 = one_eighth_note(c2, POINT(0, 0));
+	HLayer note3 = one_eighth_note(c3, POINT(0, 0));
+	HLayer note4 = one_eighth_note(c4, POINT(0, 0));
 
 	layer_composite(layer, note1, p1, NULL);
 	layer_composite(layer, note2, p2, NULL);
--- components/k2511070/test/example_a.c
+++ components/k2511070/test/example_a.c
@@ -1,11 +1,46 @@
 #include "k2511070/component.h"
 
-int main(){
-    double time = 1.0;
-    HLayer layer = k2511070_bound_one_note_layer(time);
-    if(layer != NULL){
-        layer_save_p3(layer,"test_output_layer1.ppm");
-        layer_destroy(layer);
-    }
-    return 0;
+/// @file main.c
+/// @brief 動画コンポーネント統合アプリケーション
+#include "layer/layer.h"
+#include <stdio.h>
+
+typedef HLayer(*F)(double);
+
+int main(void)
+{
+    printf("aaa\n");
+	F f[] = {k2511070_bound_one_note_layer};
+
+	HLayer layer = layer_create(SIZE(640, 480));
+
+	double diff = 1 / 30.0;
+	for (int j = 0; j*diff<=5.0; ++j)
+	{
+        double t = j*diff;
+        layer_fill(
+			layer,
+			(RGBA){
+				{1.0, 1.0, 1.0},
+				1.0
+        }
+		);
+
+		for (size_t i = 0; i < sizeof(f)/sizeof(f[0]); i++)
+		{
+			HLayer src = f[i](t);
+			layer_composite(layer, src, POINT(0, 0), NULL);
+			layer_destroy(src);
+		}
+
+		char filename[256];
+		snprintf(filename, sizeof(filename), "output/frames/frame_%04d.ppm", j);
+
+		printf("Saving %s...\n", filename);
+		layer_save_p3(layer, filename);
+	}
+
+	layer_destroy(layer);
+
+	return 0;
 }
